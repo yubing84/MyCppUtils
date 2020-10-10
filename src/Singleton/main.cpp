b80d75dfa828b483b120b06a7118164c5d32d9f8
HW140701 <623256052@qq.com>
2020-10-10 14:20:23 +0800

    1 增加单例模板使用示例

---
+++ src/Singleton/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Singleton.h"
+
+using namespace std;
+
+class A
+{
+public:
+	typedef std::shared_ptr<A> ptr;
+	A()
+	{
+		std::cout << "¹¹Ôì" << std::endl;
+		m_Value = 10;
+	};
+
+	virtual ~A()
+	{
+
+	};
+public:
+	int m_Value;
+};
+
+int main()
+{
+	A::ptr m_pA = Singleton<A>::GetInstance();
+
+	std::cout << "m_Value = " << m_pA->m_Value << std::endl;
+
+	std::cout << "m_Value2 = " << Singleton<A>::GetInstance()->m_Value << std::endl;
+
+	getchar();
+
+	return 0;
+}
